1d95a32622b599432ecbaddf6954e3466dee610b
Nodirbek Azizov <azizovv.nodirbek@gmail.com>
2022-07-15 16:59:21 +0500

    feat: Models are created

---
+++ src/BudgetManager.Shared/BudgetManager.Shared.Models.MongoDB/Entities/Category.cs
@@ -0,0 +1,24 @@
+using BudgetManager.Shared.Models.MongoDB.Enums;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BudgetManager.Shared.Models.MongoDB.Entities
+{    
+    public class Category
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Limit { get; set; }
+        [Required]
+        public LimitPeriods LimitPeriod { get; set; }
+        [BsonId]
+        public List<Guid>? SubCategories { get; set; }
+        [Required]
+        public CategoryTypes CategoryType { get; set; }
+        [Required]
+        public string Color { get; set; }
+    }
+}
