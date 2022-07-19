738ad47c6aba13dec380e23968f8f1919c208455
Sokhibjon Zamonov <sokhibjonzamonov@gmail.com>
2022-07-19 14:09:04 +0500

    Configured connection to MongoDb

---
+++ src/BudgetManager/BudgetManager.DataAccess/DatabaseSettings/IMongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetManager.DataAccess.DatabaseSettings
+{
+    public interface IMongoDbSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ src/BudgetManager/BudgetManager.DataAccess/DatabaseSettings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetManager.DataAccess.DatabaseSettings
+{
+    public class MongoDbSettings : IMongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
