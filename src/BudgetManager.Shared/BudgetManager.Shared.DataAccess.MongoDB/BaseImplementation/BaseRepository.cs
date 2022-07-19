6c49cf3459f8b3d23dd0471c578c77b25930fec4
Nodirbek Azizov <azizovv.nodirbek@gmail.com>
2022-07-19 15:50:38 +0500

    feat: basic repository implementation

---
+++ src/BudgetManager.Shared/BudgetManager.Shared.DataAccess.MongoDB/BaseImplementation/BaseRepository.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq.Expressions;
+using BudgetManager.Shared.Models.MongoDB.Models.Interfaces;
+using MongoDB.Driver;
+using MongoDbGenericRepository.Attributes;
+
+namespace BudgetManager.Shared.DataAccess.MongoDB.BaseImplementation
+{
+    public class BaseRepository<TDocument> : IBaseRepository<TDocument>
+        where TDocument : IModelBase
+    {
+        private readonly IMongoCollection<TDocument> _collection;
+        public BaseRepository(IMongoDbSettings settings)
+        {
+            var database = new MongoClient(settings.Value.ConnectionString).GetDatabase(settings.Value.Name);
+            _collection = database.GetCollection<TDocument>(GetCollectionName(typeof(TDocument)));
+        }
+
+        public virtual IQueryable<TDocument> AsQueryable()
+        {
+            return _collection.AsQueryable();
+        }
+
+        public virtual IEnumerable<TProjected> FilterBy<TProjected>(
+        Expression<Func<TDocument, bool>> filterExpression,
+        Expression<Func<TDocument, TProjected>> projectionExpression)
+        {
+            return _collection.Find(filterExpression).Project(projectionExpression).ToEnumerable();
+        }
+
+        public virtual IEnumerable<TDocument> FilterBy(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return _collection.Find(filterExpression).ToEnumerable();
+        }
+
+        public virtual Task InsertOneAsync(TDocument document)
+        {
+            return Task.Run(() => _collection.InsertOneAsync(document));
+        }
+
+        public virtual Task<TDocument> FindByIdAsync(Guid id)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, id);
+                return _collection.Find(filter).SingleOrDefaultAsync();
+            });
+        }
+
+        public virtual async Task ReplaceOneAsync(TDocument document)
+        {
+            var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, document.Id);
+            await _collection.FindOneAndReplaceAsync(filter, document);
+        }
+
+        public virtual Task DeleteByIdAsync(Guid id)
+        {
+            return Task.Run(() =>
+            {
+                var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, id);
+                _collection.FindOneAndDeleteAsync(filter);
+            });
+        }
+
+        public virtual Task DeleteOneAsync(Expression<Func<TDocument, bool>> filterExpression)
+        {
+            return Task.Run(() => _collection.FindOneAndDeleteAsync(filterExpression));
+        }
+
+        private protected string GetCollectionName(Type documentType)
+        {
+            return ((CollectionNameAttribute)documentType.GetCustomAttributes(typeof(CollectionNameAttribute), true)
+                .FirstOrDefault())?.Name;
+        }
+    }
+}
