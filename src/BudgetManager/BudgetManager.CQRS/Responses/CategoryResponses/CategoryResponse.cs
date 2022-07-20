ecca3a1f503906c9ae0c4d8b89286975643e452f
otash8110 <otash_baxadirov@mail.ru>
2022-07-20 18:58:02 +0500

    feat: adding folders structure for cqrs

---
+++ src/BudgetManager/BudgetManager.CQRS/Responses/CategoryResponses/CategoryResponse.cs
@@ -0,0 +1,21 @@
+﻿using BudgetManager.Model.Enums;
+
+namespace BudgetManager.CQRS.Responses.CategoryResponses
+{
+    public class CategoryResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Limit { get; set; }
+
+        public LimitPeriods LimitPeriod { get; set; }
+
+        public List<Guid>? SubCategories { get; set; }
+
+        public CategoryTypes CategoryType { get; set; }
+
+        public string Color { get; set; }
+    }
+}
