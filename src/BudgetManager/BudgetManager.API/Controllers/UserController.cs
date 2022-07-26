991055ab56b5024559dbc914fbdc0ea526a7b2f1
Sokhibjon Zamonov <sokhibjonzamonov@gmail.com>
2022-07-26 15:49:26 +0500

    feat: created an API controller for user crud operations

---
+++ src/BudgetManager/BudgetManager.API/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using BudgetManager.CQRS.Commands.UserCommands;
+using BudgetManager.CQRS.Queries.UserQueries;
+using BudgetManager.SDK.DTO.UserDTOs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers(CancellationToken cancellationToken)
+        {
+            var users = await _mediator.Send(new GetUsersQuery(), cancellationToken);
+
+            return users is not null ? Ok(users) : NotFound();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(Guid id, CancellationToken cancellationToken)
+        {
+            var user = await _mediator.Send(new GetUserByIdQuery(id), cancellationToken);
+
+            return user is not null ? Ok(user) : NotFound();
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<IActionResult> AddNewUser([FromBody] AddUserDTO userDTO, CancellationToken cancellationToken)
+        {
+            var user = await _mediator.Send(new AddUserCommand(userDTO), cancellationToken);
+
+            return user == Guid.Empty ? BadRequest() : Ok();
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody] UpdateUserDTO updateUser, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new UpdateUserCommand(updateUser), cancellationToken);
+
+            return result is not null ? Ok(result) : NotFound();
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+        {
+            return await _mediator.Send(new DeleteUserCommand(id), cancellationToken) ? Ok() : NotFound();
+        }
+    }
+}
