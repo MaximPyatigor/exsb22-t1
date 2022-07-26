6ca8bd0c7f1bf36ca77cde134c04ffe0b789aab4
Saigunas <saigunasb777@gmail.com>
2022-07-26 19:52:03 +0300

    fix: if insertion fails, return null. Configure mapper in AddNotificationHandler

--- src/BudgetManager/BudgetManager.CQRS/Handlers/NotificationHandlers/AddNotificationHandler.cs
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/NotificationHandlers/AddNotificationHandler.cs
@@ -1,4 +1,5 @@
-﻿using BudgetManager.CQRS.Commands.NotificationCommands;
+﻿using AutoMapper;
+using BudgetManager.CQRS.Commands.NotificationCommands;
 using BudgetManager.CQRS.Responses.NotificationResponses;
 using BudgetManager.Model;
 using BudgetManager.Model.Enums;
@@ -10,22 +11,20 @@ namespace BudgetManager.CQRS.Handlers.NotificationHandlers
     public class AddNotificationHandler : IRequestHandler<AddNotificationCommand, string>
     {
         private readonly IBaseRepository<Notification> _dataAccess;
+        private readonly IMapper _mapper;
 
-        public AddNotificationHandler(IBaseRepository<Notification> dataAccess)
+        public AddNotificationHandler(IBaseRepository<Notification> dataAccess, IMapper mapper)
         {
             _dataAccess = dataAccess;
+            _mapper = mapper;
         }
         public async Task<string> Handle(AddNotificationCommand request, CancellationToken cancellationToken)
         {
             if (request == null) { return null; }
-            Notification notification = new Notification()
-            {
-                NotificationType = (NotificationTypes)request.notificationDto.NotificationType,
-                Description = request.notificationDto.Description,
-            };
+            var notification = _mapper.Map<Notification>(request.NotificationDto);
 
             await _dataAccess.InsertOneAsync(notification, cancellationToken);
-            return notification.Id.ToString();
+            return notification.Id == Guid.Empty ? null : notification.Id.ToString();
         }
     }
 }
