14f265bcd1755ae973a2aca6c2b41b1d0cd3cd4d
Saigunas <saigunasb777@gmail.com>
2022-07-27 18:57:59 +0300

    feat: set up seeding base seeding architecture and dependencies. Seed notification with description {Seed test2}

--- src/BudgetManager/BudgetManager.API/Program.cs
+++ src/BudgetManager/BudgetManager.API/Program.cs
@@ -1,4 +1,5 @@
 using BudgetManager.CQRS.Mapping;
+using BudgetManager.CQRS.Seeding;
 using BudgetManager.DataAccess.MongoDbAccess.Repositories;
 using BudgetManager.Model;
 using BudgetManager.Shared.DataAccess.MongoDB.BaseImplementation;
@@ -22,12 +23,14 @@ builder.Services.AddSingleton<IMongoClient, MongoClient>(sp =>
     BsonDefaults.GuidRepresentation = GuidRepresentation.Standard;
     return new MongoClient(mongoDbConfig.ConnectionString);
 });
-builder.Services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
 
+builder.Services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
 builder.Services.AddScoped<IBaseRepository<User>, UserRepository>();
 builder.Services.AddScoped<IBaseRepository<Wallet>, WalletRepository>();
 builder.Services.AddScoped<IBaseRepository<Notification>, NotificationRepository>();
 builder.Services.AddScoped<IBaseRepository<Transaction>, TransactionRepository>();
+
+builder.Services.AddScoped<ISeedingService, SeedingService>();
 
 builder.Services.AddMediatR(typeof(MappingProfile).Assembly);
 builder.Services.AddAutoMapper(typeof(MappingProfile));
@@ -49,6 +52,11 @@ builder.Services.AddCors(opt =>
 });
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var someService = scope.ServiceProvider.GetRequiredService<ISeedingService>();
+someService.Seed();
+scope.Dispose();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
---
+++ src/BudgetManager/BudgetManager.CQRS/Seeding/ISeedingService.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetManager.CQRS.Seeding
+{
+    public interface ISeedingService
+    {
+        void Seed();
+    }
+}
