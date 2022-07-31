9a04eaadcfb9d9af0251c172286738e2b0a4a441
Sokhibjon Zamonov <sokhibjonzamonov@gmail.com>
2022-07-31 17:27:40 +0500

    fix: pagination with mongodb query language

--- src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/GetTransactionsByPageHandler.cs
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/GetTransactionsByPageHandler.cs
@@ -1,26 +1,33 @@
 ﻿using BudgetManager.CQRS.Queries.TransactionQueries;
 using BudgetManager.Model;
 using BudgetManager.Shared.DataAccess.MongoDB.BaseImplementation;
+using BudgetManager.Shared.DataAccess.MongoDB.DatabaseSettings;
 using MediatR;
+using MongoDB.Driver;
 
 namespace BudgetManager.CQRS.Handlers.TransactionHandlers
 {
     public class GetTransactionsByPageHandler : IRequestHandler<GetTransactionsByPageQuery, IEnumerable<Transaction>>
     {
-        private readonly IBaseRepository<Transaction> _repository;
-
-        public GetTransactionsByPageHandler(IBaseRepository<Transaction> repository)
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<Transaction> _transactionsCollection;
+        
+        public GetTransactionsByPageHandler(IMongoDbSettings settings, IMongoClient client)
         {
-            _repository = repository;
+            _client = client;
+            _database = client.GetDatabase(settings.DatabaseName);
+            _transactionsCollection = _database.GetCollection<Transaction>(nameof(Transaction));
         }
 
         public async Task<IEnumerable<Transaction>> Handle(GetTransactionsByPageQuery request, CancellationToken cancellationToken)
         {
             const int itemsPerPage = 5;
-            int pageNumber = request.pageNumber;
-            var transactions = await _repository.GetAllAsync(cancellationToken);
+            var query = _transactionsCollection.Find(Builders<Transaction>.Filter.Empty);
 
-            return transactions.Skip(itemsPerPage * (pageNumber - 1)).Take(itemsPerPage);
+            return await query.Skip(itemsPerPage * (request.pageNumber - 1))
+                              .Limit(itemsPerPage)
+                              .ToListAsync(cancellationToken);
         }
     }
 }
