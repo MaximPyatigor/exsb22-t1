b8a81e63325efffa9b0081d1ec35ebd77c4fc00d
Paulius Truikys <paulius.truikys@gmail.com>
2022-08-02 15:20:10 +0300

    feat: add default categories when creating multiple users

--- src/BudgetManager/BudgetManager.CQRS/Handlers/UserHandlers/AddManyUsersHandler.cs
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/UserHandlers/AddManyUsersHandler.cs
@@ -9,11 +9,13 @@ namespace BudgetManager.CQRS.Handlers.UserHandlers
 {
     public class AddManyUsersHandler : IRequestHandler<AddManyUsersCommand, IEnumerable<Guid>>
     {
+        private readonly IBaseRepository<DefaultCategory> _defaultCategoryRepository;
         private readonly IBaseRepository<User> _userRepository;
         private readonly IMapper _mapper;
 
-        public AddManyUsersHandler(IBaseRepository<User> userRepository, IMapper mapper)
+        public AddManyUsersHandler(IBaseRepository<DefaultCategory> defaultCategoryRepository, IBaseRepository<User> userRepository, IMapper mapper)
         {
+            _defaultCategoryRepository = defaultCategoryRepository;
             _userRepository = userRepository;
             _mapper = mapper;
         }
@@ -21,6 +23,13 @@ namespace BudgetManager.CQRS.Handlers.UserHandlers
         public async Task<IEnumerable<Guid>> Handle(AddManyUsersCommand request, CancellationToken cancellationToken)
         {
             var mappedUsers = _mapper.Map<IEnumerable<User>>(request.users);
+
+            var allDefaultCategories = await _defaultCategoryRepository.GetAllAsync(cancellationToken);
+            var mappedDefaultCategories = _mapper.Map<IEnumerable<Category>>(allDefaultCategories);
+
+            foreach (var user in mappedUsers)
+                user.Categories.AddRange(mappedDefaultCategories);
+
             await _userRepository.InsertManyAsync(mappedUsers, cancellationToken);
             var listOfIds = _mapper.Map<IEnumerable<UserResponse>>(mappedUsers)
                 .Select(u => u.Id).ToList();
