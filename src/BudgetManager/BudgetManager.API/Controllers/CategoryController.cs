5a1fb845a63fc3a69d5665d5f3ecd8c6b1c0bfb1
“[Sirojbek <Joris5969@gmail.com>
2022-08-02 19:20:09 +0500

    178-update-api-endpoints-namings

--- src/BudgetManager/BudgetManager.API/Controllers/CategoryController.cs
+++ src/BudgetManager/BudgetManager.API/Controllers/CategoryController.cs
@@ -16,38 +16,38 @@ namespace BudgetManager.API.Controllers
             _mediator = mediator;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet]
         public async Task<IActionResult> GetAll(Guid userId, CancellationToken cancellationToken)
         {
             var response = await _mediator.Send(new GetCategoriesQuery(userId), cancellationToken);
             return response == null ? NotFound() : Ok(response);
         }
 
-        [HttpGet("[action]")]
-        public async Task<IActionResult> GetById(Guid userId, Guid categoryId, CancellationToken cancellationToken)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid userId, Guid id, CancellationToken cancellationToken)
         {
-            var response = await _mediator.Send(new GetOneCategoryQuery(userId, categoryId), cancellationToken);
+            var response = await _mediator.Send(new GetOneCategoryQuery(userId, id), cancellationToken);
             return response == null ? NotFound() : Ok(response);
         }
 
-        [HttpPost("[action]")]
+        [HttpPost]
         public async Task<IActionResult> InsertOne(AddCategoryDTO category, CancellationToken cancellationToken)
         {
             var response = await _mediator.Send(new AddCategoryCommand(category), cancellationToken);
             return response == Guid.Empty ? BadRequest() : Ok(response);
         }
 
-        [HttpPut("[action]")]
+        [HttpPut]
         public async Task<IActionResult> UpdateOne(UpdateCategoryDTO category, CancellationToken cancellationToken)
         {
             var response = await _mediator.Send(new UpdateCategoryCommand(category), cancellationToken);
             return response == null ? NotFound() : Ok(response);
         }
 
-        [HttpDelete("[action]")]
-        public async Task<IActionResult> DeleteOne(Guid userId, Guid categoryId, CancellationToken cancellationToken)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOne(Guid userId, Guid id, CancellationToken cancellationToken)
         {
-            var response = await _mediator.Send(new DeleteCategoryCommand(userId, categoryId), cancellationToken);
+            var response = await _mediator.Send(new DeleteCategoryCommand(userId, id), cancellationToken);
             return response ? Ok(response) : NotFound();
         }
     }
