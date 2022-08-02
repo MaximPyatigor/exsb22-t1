743e0cc8efc13b956cc7430c9b64c6d8c0529112
Paulius Truikys <paulius.truikys@gmail.com>
2022-08-02 15:15:26 +0300

    feat: add default categories when creating user

--- src/BudgetManager/BudgetManager.CQRS/Handlers/UserHandlers/AddUserHandler.cs
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/UserHandlers/AddUserHandler.cs
@@ -9,11 +9,13 @@ namespace BudgetManager.CQRS.Handlers.UserHandlers
 {
     public class AddUserHandler : IRequestHandler<AddUserCommand, Guid>
     {
+        private readonly IBaseRepository<DefaultCategory> _defaultCategoryRepository;
         private readonly IBaseRepository<User> _userRepository;
         private readonly IMapper _mapper;
 
-        public AddUserHandler(IBaseRepository<User> userRepository, IMapper mapper)
+        public AddUserHandler(IBaseRepository<DefaultCategory> defaultCategoryRepository, IBaseRepository<User> userRepository, IMapper mapper)
         {
+            _defaultCategoryRepository = defaultCategoryRepository;
             _userRepository = userRepository;
             _mapper = mapper;
         }
@@ -22,6 +24,11 @@ namespace BudgetManager.CQRS.Handlers.UserHandlers
         {
             AddUserDTO requestUser = request.user;
             User mappedUser = _mapper.Map<User>(requestUser);
+
+            var allDefaultCategories = await _defaultCategoryRepository.GetAllAsync(cancellationToken);
+            var mappedDefaultCategories = _mapper.Map<IEnumerable<Category>>(allDefaultCategories);
+            mappedUser.Categories.AddRange(mappedDefaultCategories);
+
             await _userRepository.InsertOneAsync(mappedUser, cancellationToken);
             return mappedUser.Id;
         }
