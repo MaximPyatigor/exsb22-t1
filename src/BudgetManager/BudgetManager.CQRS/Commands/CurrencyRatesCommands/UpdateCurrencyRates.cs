a835f2f84112eb2ca9feea602a1d7a4157a138ff
Saigunas <saigunasb777@gmail.com>
2022-08-03 15:15:34 +0300

    feat: Create CurrencyRates model with CQRS operations for upserting and getting them

---
+++ src/BudgetManager/BudgetManager.CQRS/Commands/CurrencyRatesCommands/UpdateCurrencyRates.cs
@@ -0,0 +1,12 @@
+﻿using BudgetManager.Model;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.CQRS.Commands.CurrencyRatesCommands
+{
+    public record UpdateCurrencyRatesCommand(CurrencyRates CurrencyRates) : IRequest;
+}
---
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/CurrencyRatesHandlers/GetCurrencyRatesHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BudgetManager.CQRS.Queries.CurrencyRatesQueries;
+using BudgetManager.Model;
+using BudgetManager.Shared.DataAccess.MongoDB.BaseImplementation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.CQRS.Handlers.CurrencyRatesHandlers
+{
+    public class GetCurrencyRatesHandler : IRequestHandler<GetCurrencyRatesQuery, CurrencyRates>
+    {
+        private readonly IMapper _mapper;
+        private readonly IBaseRepository<CurrencyRates> _dataAccess;
+
+        public GetCurrencyRatesHandler(IMapper mapper, IBaseRepository<CurrencyRates> dataAccess)
+        {
+            _mapper = mapper;
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<CurrencyRates> Handle(GetCurrencyRatesQuery request, CancellationToken cancellationToken)
+        {
+            var result = (await _dataAccess.GetAllAsync(CancellationToken.None)).FirstOrDefault();
+            return result;
+        }
+    }
+}
