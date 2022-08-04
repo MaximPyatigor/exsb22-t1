305fd13dc0b9f9e8409dbe1efb3351d357109f88
Vlad Gromadtsov <vlad.gromadtsov@gmail.com>
2022-08-04 18:22:15 +0400

    feat: Updating wallet date of change

---
+++ src/BudgetManager/BudgetManager.CQRS/Commands/WalletCommands/UpdateWalletDateOfChangeCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace BudgetManager.CQRS.Commands.WalletCommands
+{
+    public record UpdateWalletDateOfChangeCommand(Guid userId, Guid walletId, DateTime changeDate) : IRequest<Guid>;
+}
