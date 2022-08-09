e725447b4aa41b1f1e30c2c86ad1c6b907ba9e87
“[Sirojbek <Joris5969@gmail.com>
2022-08-09 12:46:41 +0500

    fix: expense transaction update both wallets on edit

--- src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/UpdateExpenseTransactionHandler.cs
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/UpdateExpenseTransactionHandler.cs
@@ -1,44 +1,53 @@
 ﻿using AutoMapper;
 using BudgetManager.CQRS.Commands.TransactionCommands;
+using BudgetManager.CQRS.Commands.WalletCommands;
 using BudgetManager.CQRS.Responses.TransactionResponses;
 using BudgetManager.DataAccess.MongoDbAccess.Interfaces;
 using BudgetManager.Model;
+using MediatR;
 using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BudgetManager.CQRS.Handlers.TransactionHandlers
 {
     public class UpdateExpenseTransactionHandler
     {
         private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
         private readonly ITransactionRepository _dataAccess;
 
-        public UpdateExpenseTransactionHandler(ITransactionRepository dataAccess, IMapper mapper)
+        public UpdateExpenseTransactionHandler(ITransactionRepository dataAccess, IMapper mapper, IMediator mediator)
         {
             _dataAccess = dataAccess;
             _mapper = mapper;
+            _mediator = mediator;
         }
 
         public async Task<ExpenseTransactionResponse> Handle(UpdateExpenseTransactionCommand request, CancellationToken cancellationToken)
         {
-            var mappedTransaction = _mapper.Map<Transaction>(request.updateExpenseDto);
             var builder = Builders<Transaction>.Filter;
-            var filter = builder.And(builder.Eq(t => t.Id, mappedTransaction.Id),
-                builder.Eq(t => t.UserId, request.userId));
-            var update = Builders<Transaction>.Update
-                .Set(o => o.WalletId, mappedTransaction.WalletId)
-                .Set(o => o.CategoryId, mappedTransaction.CategoryId)
-                .Set(o => o.SubCategoryId, mappedTransaction.SubCategoryId)
-                .Set(o => o.Payer, mappedTransaction.Payer)
-                .Set(o => o.DateOfTransaction, mappedTransaction.DateOfTransaction)
-                .Set(o => o.Value, mappedTransaction.Value)
-                .Set(o => o.Description, mappedTransaction.Description);
+            var filter = builder.And(builder.Eq(t => t.Id, request.updateExpenseDto.Id),
+                builder.Eq(t => t.UserId, request.userId),
+                builder.Eq(t => t.TransactionType, Model.Enums.OperationType.Expense));
 
-            var response = _mapper.Map<ExpenseTransactionResponse>(await _dataAccess.UpdateOneAsync(filter, update, cancellationToken));
+            var projection = Builders<Transaction>.Projection.Include(t => t.WalletId);
+            var oldIncomeTransaction = (await _dataAccess.FilterBy<Transaction>(filter, projection, cancellationToken)).FirstOrDefault();
+            if (oldIncomeTransaction == null) { throw new KeyNotFoundException("Transaction not found"); }
+            bool walletChanged = oldIncomeTransaction.WalletId != request.updateExpenseDto.WalletId;
+
+            var update = Builders<Transaction>.Update
+                .Set(o => o.WalletId, request.updateExpenseDto.WalletId)
+                .Set(o => o.CategoryId, request.updateExpenseDto.CategoryId)
+                .Set(o => o.SubCategoryId, request.updateExpenseDto.SubCategoryId)
+                .Set(o => o.Payer, request.updateExpenseDto.Payer)
+                .Set(o => o.DateOfTransaction, request.updateExpenseDto.DateOfTransaction)
+                .Set(o => o.Value, request.updateExpenseDto.Value)
+                .Set(o => o.Description, request.updateExpenseDto.Description);
+
+            var response = await _dataAccess.UpdateOneAsync(filter, update, cancellationToken);
+            if (response == null) { throw new KeyNotFoundException("Transaction not found"); }
+
+            await _mediator.Send(new UpdateWalletDateOfChangeCommand(request.userId, response.WalletId, DateTime.UtcNow), cancellationToken);
+            if (walletChanged) { await _mediator.Send(new UpdateWalletDateOfChangeCommand(request.userId, oldIncomeTransaction.WalletId, DateTime.UtcNow), cancellationToken); }
 
             return _mapper.Map<ExpenseTransactionResponse>(response);
         }
