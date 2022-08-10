0027ad3cf97eba776bd0a8a9de91ba8fa4cf7c5e
otash8110 <otash_baxadirov@mail.ru>
2022-08-10 19:14:32 +0500

    feat: implement walletBalanceChange handler and apply it to create expense

---
+++ src/BudgetManager/BudgetManager.CQRS/Commands/WalletCommands/ChangeTotalBalanceOfWalletCommand.cs
@@ -0,0 +1,7 @@
+﻿using BudgetManager.Model;
+using MediatR;
+
+namespace BudgetManager.CQRS.Commands.WalletCommands
+{
+    public record ChangeTotalBalanceOfWalletCommand(Transaction transaction) : IRequest<bool>;
+}
