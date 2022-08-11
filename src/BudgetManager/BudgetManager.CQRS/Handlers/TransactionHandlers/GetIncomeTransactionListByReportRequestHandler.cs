4fc29055a096fceca9ddf32c0118f2487c685c24
Saigunas <saigunasb777@gmail.com>
2022-08-11 19:06:40 +0300

    fix: add TransactionListByReport filter walletId

--- src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/GetIncomeTransactionListByReportRequestHandler.cs
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/GetIncomeTransactionListByReportRequestHandler.cs
@@ -23,12 +23,13 @@ namespace BudgetManager.CQRS.Handlers.TransactionHandlers
         public async Task<IEnumerable<Transaction>> Handle(GetIncomeTransactionListByReportRequestQuery request, CancellationToken cancellationToken)
         {
             var userFilter = Builders<Transaction>.Filter.Eq(t => t.UserId, request.UserId);
+            var walletFilter = Builders<Transaction>.Filter.Eq(t => t.WalletId, request.ReportRequestInfo.WalletId);
             var dateFromFilter = Builders<Transaction>.Filter.Gte(t => t.DateOfTransaction, request.ReportRequestInfo.DateFrom);
             var dateToFilter = Builders<Transaction>.Filter.Lte(t => t.DateOfTransaction, request.ReportRequestInfo.DateTo);
             var categoriesFilter = Builders<Transaction>.Filter.In(t => t.CategoryId, request.ReportRequestInfo.IncomeCategoryIds);
             var transactionTypeFilter = Builders<Transaction>.Filter.Eq(t => t.TransactionType, OperationType.Income);
 
-            var filter = Builders<Transaction>.Filter.And(userFilter, dateFromFilter, dateToFilter, categoriesFilter, transactionTypeFilter);
+            var filter = Builders<Transaction>.Filter.And(userFilter, walletFilter, dateFromFilter, dateToFilter, categoriesFilter, transactionTypeFilter);
 
             return await _dataAccess.FilterBy(filter, cancellationToken);
         }
