a0a990a674dcde79fa6bb68d8c9408e4c2ae559c
Vlad Gromadtsov <vlad.gromadtsov@gmail.com>
2022-08-11 11:38:01 +0400

    fix: delete unnecessary method

--- src/BudgetManager/BudgetManager.DataAccess/MongoDbAccess/Interfaces/ITransactionRepository.cs
+++ src/BudgetManager/BudgetManager.DataAccess/MongoDbAccess/Interfaces/ITransactionRepository.cs
@@ -10,7 +10,6 @@ namespace BudgetManager.DataAccess.MongoDbAccess.Interfaces
         Task<IEnumerable<Transaction>> GetListByOperationAsync(Guid userId, OperationType operationType, CancellationToken cancellationToken);
         Task<IEnumerable<Transaction>> GetListByUserIdAsync(Guid userId, CancellationToken cancellationToken);
         Task<bool> DeleteManyByUserIdAsync(Guid userId, CancellationToken cancellationToken);
-        Task<(IEnumerable<Transaction>, long)> GetPageListByOperationAsync(Guid userId, OperationType operationType, int page, int pageSize, CancellationToken cancellationToken);
         Task<(IEnumerable<Transaction>, long)> GetPageListAsync(FilterDefinition<Transaction> filterDefinition,
             SortDefinition<Transaction> sortDefinition, int page, int pageSize, CancellationToken cancellationToken);
     }
--- src/BudgetManager/BudgetManager.DataAccess/MongoDbAccess/Repositories/TransactionRepository.cs
+++ src/BudgetManager/BudgetManager.DataAccess/MongoDbAccess/Repositories/TransactionRepository.cs
@@ -37,21 +37,6 @@ namespace BudgetManager.DataAccess.MongoDbAccess.Repositories
             return result;
         }
 
-        public async Task<(IEnumerable<Transaction>, long)> GetPageListByOperationAsync(Guid userId, OperationType operationType, int page, int pageSize, CancellationToken cancellationToken)
-        {
-            var filterUser = Builders<Transaction>.Filter.Eq(x => x.UserId, userId);
-            var filterOperation = Builders<Transaction>.Filter.Eq(x => x.TransactionType, operationType);
-            var filter = Builders<Transaction>.Filter.And(filterUser, filterOperation);
-
-            var filtred = _collection.Find(filter);
-
-            var count = await filtred.CountAsync(cancellationToken);
-            var result = await filtred.SortBy(x => x.DateOfTransaction)
-                .Skip(pageSize * (page - 1)).Limit(pageSize).ToListAsync(cancellationToken);
-
-            return (result, count);
-        }
-
         public async Task<(IEnumerable<Transaction>, long)> GetPageListAsync(FilterDefinition<Transaction> filterDefinition,
             SortDefinition<Transaction> sortDefinition,  int page, int pageSize, CancellationToken cancellationToken)
         {
