2ccba188808e665083f387a9808b505144d35986
otash8110 <otash_baxadirov@mail.ru>
2022-08-11 17:19:51 +0500

    feat: get total balance implemented

---
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/UserHandlers/CalculateUserTotalBalanceHandler.cs
@@ -0,0 +1,42 @@
+﻿using BudgetManager.CQRS.Queries.CurrencyRatesQueries;
+using BudgetManager.CQRS.Queries.UserQueries;
+using BudgetManager.CQRS.Queries.WalletQueries;
+using BudgetManager.Model;
+using BudgetManager.Shared.DataAccess.MongoDB.BaseImplementation;
+using MediatR;
+using MongoDB.Driver;
+
+namespace BudgetManager.CQRS.Handlers.UserHandlers
+{
+    public class CalculateUserTotalBalanceHandler : IRequestHandler<CalculateUserTotalBalanceQuery, decimal>
+    {
+        private readonly IBaseRepository<User> _userRepository;
+        private readonly IMediator _mediator;
+
+        public CalculateUserTotalBalanceHandler(IBaseRepository<User> userRepository,
+            IMediator mediator)
+        {
+            _userRepository = userRepository;
+            _mediator = mediator;
+        }
+
+        public async Task<decimal> Handle(CalculateUserTotalBalanceQuery request, CancellationToken cancellationToken)
+        {
+            var convertTo = request.currencyCode;
+            var userId = request.userId;
+            decimal totalBalance = 0;
+            var userWallets = await _mediator.Send(new GetWalletListQuery(userId), cancellationToken);
+
+            foreach (var wallet in userWallets)
+            {
+                var amount = await _mediator.Send(
+                    new ConvertCurrencyRatesQuery(wallet.Currency.CurrencyCode, convertTo, wallet.Balance),
+                    cancellationToken);
+
+                totalBalance += amount;
+            }
+
+            return totalBalance;
+        }
+    }
+}
---
+++ src/BudgetManager/BudgetManager.CQRS/Queries/UserQueries/CalculateUserTotalBalanceQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace BudgetManager.CQRS.Queries.UserQueries
+{
+    public record CalculateUserTotalBalanceQuery(Guid userId, string currencyCode) : IRequest<decimal>;
+}
