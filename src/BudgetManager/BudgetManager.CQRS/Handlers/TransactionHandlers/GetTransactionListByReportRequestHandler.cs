a0721e947b1f131f9378be4f610da4b6fef5736a
Saigunas <saigunasb777@gmail.com>
2022-08-12 11:22:17 +0300

    feat: added getExpenseTransaction by report handler

---
+++ src/BudgetManager/BudgetManager.CQRS/Handlers/TransactionHandlers/GetTransactionListByReportRequestHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BudgetManager.CQRS.Queries.TransactionQueries;
+using BudgetManager.CQRS.Responses.TransactionResponses;
+using BudgetManager.DataAccess.MongoDbAccess.Interfaces;
+using BudgetManager.Model;
+using BudgetManager.Model.Enums;
+using MediatR;
+using MongoDB.Driver;
+
+namespace BudgetManager.CQRS.Handlers.TransactionHandlers
+{
+    public class GetTransactionListByReportRequestHandler : IRequestHandler<GetTransactionListByReportRequestQuery, IEnumerable<Transaction>>
+    {
+        private readonly IMapper _mapper;
+        private readonly ITransactionRepository _dataAccess;
+
+        public GetTransactionListByReportRequestHandler(IMapper mapper, ITransactionRepository dataAccess)
+        {
+            _mapper = mapper;
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<IEnumerable<Transaction>> Handle(GetTransactionListByReportRequestQuery request, CancellationToken cancellationToken)
+        {
+            var userFilter = Builders<Transaction>.Filter.Eq(t => t.UserId, request.UserId);
+            var walletFilter = Builders<Transaction>.Filter.Eq(t => t.WalletId, request.ReportRequestInfo.WalletId);
+            var dateFromFilter = Builders<Transaction>.Filter.Gte(t => t.DateOfTransaction, request.ReportRequestInfo.DateFrom);
+            var dateToFilter = Builders<Transaction>.Filter.Lte(t => t.DateOfTransaction, request.ReportRequestInfo.DateTo);
+            var categoriesFilter = Builders<Transaction>.Filter.In(t => t.CategoryId, request.ReportRequestInfo.IncomeCategoryIds);
+            var transactionTypeFilter = Builders<Transaction>.Filter.Eq(t => t.TransactionType, request.TransactionType);
+
+            var filter = Builders<Transaction>.Filter.And(userFilter, walletFilter, dateFromFilter, dateToFilter, categoriesFilter, transactionTypeFilter);
+
+            return await _dataAccess.FilterBy(filter, cancellationToken);
+        }
+    }
+}
---
+++ src/BudgetManager/BudgetManager.CQRS/Queries/TransactionQueries/GetTransactionListByReportRequestQuery.cs
@@ -0,0 +1,14 @@
+﻿using BudgetManager.Model;
+using BudgetManager.Model.Enums;
+using BudgetManager.Model.ReportModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.CQRS.Queries.TransactionQueries
+{
+    public record GetTransactionListByReportRequestQuery(Guid UserId, ReportRequest ReportRequestInfo, OperationType TransactionType) : IRequest<IEnumerable<Transaction>>;
+}
