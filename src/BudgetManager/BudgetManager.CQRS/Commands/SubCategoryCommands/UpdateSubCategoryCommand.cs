10af5dba1ff40d566590bbcf324d716544d77e74
Paulius Truikys <paulius.truikys@gmail.com>
2022-08-15 10:19:41 +0300

    feat: update SubCategory functionality

---
+++ src/BudgetManager/BudgetManager.CQRS/Commands/SubCategoryCommands/UpdateSubCategoryCommand.cs
@@ -0,0 +1,8 @@
+﻿using BudgetManager.CQRS.Responses.SubCategoryResponses;
+using BudgetManager.SDK.DTOs;
+using MediatR;
+
+namespace BudgetManager.CQRS.Commands.SubCategoryCommands
+{
+    public record UpdateSubCategoryCommand(Guid userId, UpdateSubCategoryDTO updateSubCategoryDTO) : IRequest<SubCategoryResponse>;
+}
